db5a49bc4df3df80761b988f412af4de1b324f57
RadheMohan Chaudhary <radhechy0412@gmail.com>
2024-03-09 14:32:23 +0545

    RADHE

--- flutter-learning/lib/pages/recommend.dart
+++ flutter-learning/lib/pages/recommend.dart
@@ -85,43 +85,174 @@ class _recommend extends StatelessWidget {
           color: Colors.transparent,
         ),
         Padding(
-          padding: EdgeInsets.all(60),
+          padding: EdgeInsets.only(top: 50, bottom: 10, left: 60, right: 60),
           child: Text(
-            "According to your parameter, The best foods we would like to recommend you are given below.",
+            "The recommended food items for your mood (Happy) are:",
             textAlign: TextAlign.center,
             style: TextStyle(
-              fontSize: 14,
+              fontSize: 16,
               fontWeight: FontWeight.w600,
               color: Colors.black,
             ),
           ),
         ),
-        Text(
-          "Food Name",
-          textAlign: TextAlign.center,
-          style: TextStyle(
-            fontSize: 24,
-            fontWeight: FontWeight.bold,
-            color: Colors.black,
-          ),
-        ),
-        SizedBox(
-          height: 60,
+        Placeholder(
+          fallbackHeight: 150,
+          color: Colors.transparent,
+          child: _items(),
         ),
         Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: ElevatedButton(
-              onPressed: () {
-                Navigator.pushNamed(context, MyRoutes.restaurantsRoute);
-              },
-              style: ElevatedButton.styleFrom(
-                fixedSize: Size(400, 45),
-                primary: Color(0xff302e2d),
-                onPrimary: Colors.white,
-              ),
-              child: Text("Get Food")),
+          padding: EdgeInsets.only(top: 0, bottom: 25, left: 60, right: 60),
+          child: Text(
+            "Some restaurants offering these dishes are:",
+            textAlign: TextAlign.center,
+            style: TextStyle(
+              fontSize: 16,
+              fontWeight: FontWeight.w600,
+              color: Colors.black,
+            ),
+          ),
         ),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 40),
+          child: Placeholder(
+            fallbackHeight: 400,
+            color: Colors.transparent,
+            child: _restaurant(),
+          ),
+        )
       ],
+    );
+  }
+}
+
+class _items extends StatelessWidget {
+  const _items({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+        itemCount: 3,
+        itemBuilder: (context, index) => Padding(
+              padding: const EdgeInsets.all(10),
+              child: Text(
+                "Item Name",
+                textAlign: TextAlign.center,
+                style: TextStyle(
+                  fontSize: 14,
+                  fontWeight: FontWeight.normal,
+                ),
+              ),
+            ));
+  }
+}
+
+class _restaurant extends StatelessWidget {
+  const _restaurant({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ListView.builder(
+      itemCount: 3,
+      itemBuilder: (context, index) => Padding(
+        padding: const EdgeInsets.all(2.5),
+        child: Column(
+          children: [
+            Container(
+              height: 70,
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(20),
+                color: Colors.white,
+              ),
+              child: Padding(
+                padding: const EdgeInsets.all(10),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  children: [
+                    Row(
+                      children: [
+                        Padding(
+                          padding: const EdgeInsets.all(5),
+                          child: Icon(
+                            CupertinoIcons.star,
+                            size: 20,
+                            color: Color(0xfffdb600),
+                          ),
+                        ),
+                        Text(
+                          "4.0",
+                          style: TextStyle(
+                              fontSize: 16,
+                              color: Colors.black,
+                              fontWeight: FontWeight.w500),
+                        )
+                      ],
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(right: 30),
+                      child: Column(
+                        mainAxisAlignment: MainAxisAlignment.center,
+                        crossAxisAlignment: CrossAxisAlignment.start,
+                        children: [
+                          Text(
+                            "Restaurant Name",
+                            style: TextStyle(
+                              fontSize: 16,
+                              fontWeight: FontWeight.w600,
+                              color: Colors.black,
+                            ),
+                          ),
+                          Row(
+                            children: [
+                              Opacity(
+                                opacity: 0.5,
+                                child: Text(
+                                  "City Name",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              Opacity(
+                                opacity: 0.5,
+                                child: Text(
+                                  ",",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                              Opacity(
+                                opacity: 0.5,
+                                child: Text(
+                                  "Street Name",
+                                  style: TextStyle(
+                                    fontSize: 12,
+                                    color: Colors.black,
+                                  ),
+                                ),
+                              ),
+                            ],
+                          )
+                        ],
+                      ),
+                    ),
+                    InkWell(
+                      onTap: () {
+                        Navigator.pushNamed(context, MyRoutes.itemsRoute);
+                      },
+                      child: Icon(CupertinoIcons.forward),
+                    )
+                  ],
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
